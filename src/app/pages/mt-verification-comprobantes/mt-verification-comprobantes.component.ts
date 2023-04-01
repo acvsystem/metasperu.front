6f18f936f2d71742c1e653848b53fcc28fd7e941
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-04-01 11:12:16 -0500

    update

--- src/app/pages/mt-verification-comprobantes/mt-verification-comprobantes.component.ts
+++ src/app/pages/mt-verification-comprobantes/mt-verification-comprobantes.component.ts
@@ -24,25 +24,35 @@ export class MtVerificationComprobantesComponent implements OnInit {
   ngOnInit() {
     const self = this;
     this.headList = ['#', 'Codigo', 'Tienda', 'Verificacion', 'Comprobantes', 'Online', 'Server ICG']
-    this.headListSunat = ['#', 'Codigo Documento', 'Nro Correlativo', 'Nom Adquiriente', 'Num documento', 'Tipo documento adq.', 'Observacion', 'Estado Sunat','Estado Comprobante', 'Codigo sunat', 'Fecha emision']
+    this.headListSunat = ['#', 'Codigo Documento', 'Nro Correlativo', 'Nom Adquiriente', 'Num documento', 'Tipo documento adq.', 'Observacion', 'Estado Sunat', 'Estado Comprobante', 'Codigo sunat', 'Fecha emision']
 
     this.socket.on('sendNotificationSunat', (sunat) => {
       let dataList = [];
       dataList = sunat || [];
       this.bodyListSunat = [];
       (dataList || []).filter((dataSocket: any) => {
-        (this.bodyListSunat || []).push({
-          cod_documento: (dataSocket || {}).CODIGO_DOCUMENTO,
-          nro_correlativo: (dataSocket || {}).NRO_CORRELATIVO,
-          nom_aquiriente: (dataSocket || {}).NOM_ADQUIRIENTE,
-          nro_documento: (dataSocket || {}).NRO_DOCUMENTO,
-          tipo_doc_adquiriente: (dataSocket || {}).TIPO_DOCUMENTO_ADQUIRIENTE,
-          observacion: (dataSocket || {}).OBSERVACION,
-          estado_sunat: (dataSocket || {}).ESTADO_SUNAT,
-          estado_comprobante: (dataSocket || {}).ESTADO_COMPROBANTE,
-          codigo_sunat: (dataSocket || {}).CODIGO_ERROR_SUNAT,
-          fecha_emision: (dataSocket || {}).FECHA_EMISION
-        });
+        const fechaDocumento = new Date((dataSocket || {}).FECHA_EMISION).toLocaleDateString('en-CA');
+        const fecha = new Date().toLocaleDateString('en-CA');
+        var fechaInicio = new Date(fechaDocumento).getTime();
+        var fechaFin = new Date(fecha).getTime();
+
+        var diff: any = (fechaFin - fechaInicio) / (1000 * 60 * 60 * 24) || 0;
+
+        if (parseInt(diff) <= 3) {
+          (this.bodyListSunat || []).push({
+            cod_documento: (dataSocket || {}).CODIGO_DOCUMENTO,
+            nro_correlativo: (dataSocket || {}).NRO_CORRELATIVO,
+            nom_aquiriente: (dataSocket || {}).NOM_ADQUIRIENTE,
+            nro_documento: (dataSocket || {}).NRO_DOCUMENTO,
+            tipo_doc_adquiriente: (dataSocket || {}).TIPO_DOCUMENTO_ADQUIRIENTE,
+            observacion: (dataSocket || {}).OBSERVACION,
+            estado_sunat: (dataSocket || {}).ESTADO_SUNAT,
+            estado_comprobante: (dataSocket || {}).ESTADO_COMPROBANTE,
+            codigo_sunat: (dataSocket || {}).CODIGO_ERROR_SUNAT,
+            fecha_emision: (dataSocket || {}).FECHA_EMISION
+          });
+        }
+
       });
     });
 
