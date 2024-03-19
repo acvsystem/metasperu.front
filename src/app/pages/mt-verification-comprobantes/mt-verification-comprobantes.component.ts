b711a0a127478192d0d1a183a410ed02715fadb1
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-03-19 15:07:34 -0500

    update

--- src/app/pages/mt-verification-comprobantes/mt-verification-comprobantes.component.ts
+++ src/app/pages/mt-verification-comprobantes/mt-verification-comprobantes.component.ts
@@ -17,7 +17,7 @@ export class MtVerificationComprobantesComponent implements OnInit {
   isConnectServer: string = 'false';
   isVisibleStatus: boolean = false;
   statusServerList: any = [];
-  socket = io('http://192.168.1.200:3200', { query: { code: 'app' } });
+  socket = io('http://190.117.53.247:3200', { query: { code: 'app' } });
 
   constructor() { }
 
