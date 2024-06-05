054c3d940edb567b66f668c2cd573bf71adbfda9
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-06-05 14:24:19 -0500

    update

--- src/app/pages/mt-login/mt-login.component.ts
+++ src/app/pages/mt-login/mt-login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShareService } from '../../services/shareService'
+import { NavController } from '@ionic/angular';
+import { StorageService } from '../../utils/storage';
 
 @Component({
   selector: 'app-mt-login',
@@ -7,8 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MtLoginComponent implements OnInit {
 
-  constructor() { }
+  userName: string = "";
+  password: string = "";
 
-  ngOnInit() {}
+  constructor(
+    private shrService: ShareService,
+    private nav: NavController,
+    private store: StorageService
+  ) { }
+
+  ngOnInit() {
+/*
+    document.addEventListener('keydown', (event) => {
+      var keyValue = event.key;
+      if (keyValue == "Enter") {
+        this.onLogin();
+      }
+    }, false);*/
+  }
+
+
+  onLogin() {
+    this.shrService.createToken(this.userName, this.password).then((token) => {
+      if (token) {
+        this.nav.navigateRoot('comprobantes');
+      }
+    });
+  }
+
+  onChangeInput(data: any) {
+    let inputData = data || {};
+    let index = (inputData || {}).id || "";
+    this[index] = (inputData || {}).value || "";
+  }
 
 }
