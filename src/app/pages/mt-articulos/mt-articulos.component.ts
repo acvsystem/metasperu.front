ab605eb483d31421d1624991f1370c80521418cf
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-06-05 12:55:06 -0500

    update

---
+++ src/app/pages/mt-articulos/mt-articulos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mt-articulos',
+  templateUrl: './mt-articulos.component.html',
+  styleUrls: ['./mt-articulos.component.scss'],
+})
+export class MtArticulosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
