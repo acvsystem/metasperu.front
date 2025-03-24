c6137f9e23493252c5043c3bb2b61210c68b17b2
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-03-24 12:57:46 -0500

    update

--- src/app/components/mt-papeleta-horario/mt-papeleta-horario.component.ts
+++ src/app/components/mt-papeleta-horario/mt-papeleta-horario.component.ts
@@ -275,6 +275,95 @@ export class MtPapeletaHorarioComponent implements OnInit {
               });
             }
 
+            if ((huellero || {}).isException) { //UNA SOLA MARCACION TRABAJO MADRUGADA
+               
+              let indexData = (this.onDataTemp || []).findIndex((data) => ((data || {}).dia == (huellero || []).dia));
+
+              if (huellero.tpAsociado != "**") { //DEFAULT
+              
+                let hora_1_pr = this.onDataTemp[indexData]['hr_trabajadas'].split(":");
+
+                let defaultHT = "00:00";
+
+                if (tipoAsc.length == 2) { //LACTANCIA
+
+                  let fechaLactancia = new Date(tipoAsc[1]).toLocaleDateString().split('/'); new Date();
+
+                  var f1 = new Date(parseInt(fechaLactancia[2]) + 1 + "-" + fechaLactancia[1] + "-" + parseInt(fechaLactancia[0])).getTime(); //FECHA DE LACTANCIA
+                  var f2 = new Date(this.onDataTemp[indexData]['dia']).getTime(); //FECHA TRABAJADA
+
+                  if (f1 >= f2) {
+                    defaultHT = "07:00";
+                  }
+                }
+
+                let hrxLlegada = this.onDataTemp[indexData]['hr_trabajadas'].split(':');
+                let llegada = parseInt(hrxLlegada[0]) * 60 + parseInt(hrxLlegada[1]);
+                let hrxSalida = (defaultHT).split(':');
+                let salida = parseInt(hrxSalida[0]) * 60 + parseInt(hrxSalida[1]);
+
+                let newAcumulado = llegada > salida ? llegada - salida : salida - llegada;
+
+                const ToTime = (num) => {
+                  var minutos: any = Math.floor((num / 60) % 60);
+                  minutos = minutos < 10 ? '0' + minutos : minutos;
+                  var segundos: any = num % 60;
+                  segundos = segundos < 10 ? '0' + segundos : segundos;
+                  return minutos + ':' + segundos;
+                }
+
+                let process = ToTime(newAcumulado);
+
+                let fecha = new Date().toLocaleDateString().split('/'); new Date();
+
+                let validFecha = new Date(this.onDataTemp[indexData]['dia']).getTime() != new Date(parseInt(fecha[2]) + "-" + (parseInt(fecha[1]) <= 9 ? '0' + parseInt(fecha[1]) : parseInt(fecha[1])) + "-" + (parseInt(fecha[0]) <= 9 ? '0' + parseInt(fecha[0]) : parseInt(fecha[0]))).getTime() ? true : false;
+
+
+                if ((hora_1_pr[0] >= 8 && validFecha) || this.onDataTemp[indexData].isException) {
+
+                  let hr = process.split(":");
+
+                  //CONTEO HORA EXTRA
+
+                  let tolerancia = this.dataViewTolerancia.find((dtt) => dtt.REFERENCIA == 'hora extra default');
+
+
+                  if (parseInt(hr[1]) >= parseInt(((tolerancia || {}).TIEMPO_TOLERANCIA).split(":")[1]) || parseInt(hr[0]) > 0 || this.onDataTemp[indexData].isException) {
+
+                    this.onDataTemp[indexData]['hr_extra'] = process;//23:59
+
+                    let hrxSalida = this.onDataTemp[indexData]['hr_extra'].split(':');
+                    let salida = parseInt(hrxSalida[0]) * 60 + parseInt(hrxSalida[1]);
+
+                    let estado = this.onDataTemp[indexData]['dataRegistro'].length == 1 || salida >= 356 || this.onDataTemp[indexData]['hr_salida_2'] == '23:59:59' || this.onDataTemp[indexData]['hr_ingreso_1'] == '00:00:00' ? 'aprobar' : 'correcto';
+                    let ejb = this.parseEJB.filter((ejb) => ejb.documento == this.cboEmpleado);
+
+                    let aprobado = estado == "correcto" ? true : false;
+
+                    let indexData2 = (this.dataVerify || []).findIndex((data) => ((data || {}).fecha == this.onDataTemp[indexData]['dia']));
+
+                    if (indexData2 == -1) {
+                      let obj = { documento: ejb[0]['documento'], codigo_papeleta: this.codigoPapeleta, hr_trabajadas: this.onDataTemp[indexData]['hr_trabajadas'], fecha: this.onDataTemp[indexData]['dia'], hrx_acumulado: this.onDataTemp[indexData]['hr_extra'], extra: this.onDataTemp[indexData]['hr_extra'], estado: estado, aprobado: aprobado, seleccionado: false };
+                      (this.dataVerify || []).push(obj);
+                    } 
+
+                    (this.arCopiHoraExtra || []).push({ fecha: this.onDataTemp[indexData]['dia'], extra: process, estado: estado });
+
+                    if (estado == 'correcto') {
+                      if (!this.arHoraExtra.length) {
+                        this.arHoraExtra = [process];
+                      } else {
+                        this.arHoraExtra[0] = this.obtenerHorasTrabajadas(process, this.arHoraExtra[0]);
+                      }
+                    }
+                  }
+                } else {
+                  this.onDataTemp[indexData]['hr_faltante'] = process;
+                }
+
+              }
+            }
+
           } else {
 
             if (huellero.tpAsociado != "**") { //DEFAULT
