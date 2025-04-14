a5cb55730b7f7cc6b54b8f7d9d1935c4f6d5fa4d
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-04-13 20:51:45 -0500

    update kardex

--- src/app/pages/mt-kardex-contabilidad/mt-kardex-contabilidad.component.ts
+++ src/app/pages/mt-kardex-contabilidad/mt-kardex-contabilidad.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatListModule } from '@angular/material/list';
 import { ShareService } from 'src/app/services/shareService';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 interface Shoes {
   value: string;
@@ -18,7 +21,18 @@ export class MtKardexContabilidadComponent implements OnInit {
   vDetallado: Array<any> = [];
   cboListTienda: Array<any> = [];
   tiendasList: Array<any> = [];
+  dataView: Array<any> = [];
+  dataViewVenc: Array<any> = [];
+  displayedColumns: string[] = ['referencia', 'talla', 'color', 'descripcion','unid','precio','descuento','total','almacen'];
+  displayedColumnsVenc: string[] = ['forma_pago','importe','medio_pago','estado','fecha_cobro'];
+  dataSource = new MatTableDataSource<any>(this.dataView);
+  dataSourceVenc = new MatTableDataSource<any>(this.dataViewVenc);
   cboTiendaConsulting: String = "";
+  vSerieDoc: String = "9N1A";
+  vNumeroDoc: String = "";
+  vFechaDoc: String = "";
+  vHoraDoc: String = "";
+
   shoes: Shoes[] = [
     { value: 'boots', name: 'Boots' },
     { value: 'clogs', name: 'Clogs' },
